700c551aad05d34a174693636f0ea695d3a0587a
Miti Mazmudar <mitimazmudar@gmail.com>
2023-09-28 14:09:31 -0400

    fix: include PB Message 'Type' field in output PIR responses

--- handlers.go
+++ handlers.go
@@ -302,6 +302,7 @@ func (d *DHT) handlePrivateFindPeer(ctx context.Context, remote peer.ID, msg *pb
 	}
 
 	response := &pb.Message{
+		Type:             pb.Message_PRIVATE_FIND_NODE,
 		EncryptedRecords: pirResponse,
 	}
 
@@ -340,6 +341,7 @@ func (d *DHT) handlePrivateGetProviderRecords(ctx context.Context, remote peer.I
 	}
 
 	response := &pb.Message{
+		Type:             pb.Message_PRIVATE_GET_PROVIDERS,
 		EncryptedRecords: pirResponse,
 	}
 
