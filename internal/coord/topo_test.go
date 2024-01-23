c39a606f69e18439e4bb4662d0ea749245e9daff
Miti Mazmudar <mitimazmudar@gmail.com>
2024-01-23 12:27:20 -0500

    topo_test.go: rename

--- internal/coord/topo_test.go
+++ internal/coord/topo_test.go
@@ -15,26 +15,26 @@ func TestRoutingNormVsSimple(t *testing.T) {
 	// ctx := kadtest.CtxShort(t)
 
 	clk := clock.NewMock()
-	_, nodes, err := nettest.GenerateCrawledTopology(clk, true)
-	_, nodes_simple, err := nettest.GenerateCrawledTopology(clk, false)
+	_, nodesNormalizedRT, err := nettest.GenerateCrawledTopology(clk, true)
+	_, nodesSimpleRT, err := nettest.GenerateCrawledTopology(clk, false)
 	require.NoError(t, err)
 
-	// self := nodes[0].NodeID
+	// self := nodesNormalizedRT[0].NodeID
 
-	num_nodes := len(nodes)
-	fmt.Println("Number of nodes: ", num_nodes)
+	num_nodes := len(nodesNormalizedRT)
+	fmt.Println("Number of nodesNormalizedRT: ", num_nodes)
 	// select number from 0 to num_nodes-1 at random
 	// generate random integer between 0 and num_nodes-1
 	target_node := rand.Intn(num_nodes - 1)
-	target := nodes[target_node].NodeID
+	target := nodesNormalizedRT[target_node].NodeID
 
-	simple_target := nodes_simple[target_node].NodeID.Key()
+	simple_target := nodesSimpleRT[target_node].NodeID.Key()
 	require.Equal(t, target.Key(), simple_target)
 
-	rt := nodes[0].RoutingTable
+	rt := nodesNormalizedRT[0].RoutingTable
 	// define boolean to check if target is in seeds list
 	// seeds is a list of PeerIDs
-	seeds := rt.NearestNodes(target.Key(), 5) // 5 closest nodes to target <- change if needed for various experiments
+	seeds := rt.NearestNodes(target.Key(), 5) // 5 closest nodesNormalizedRT to target <- change if needed for various experiments
 	targetFound := false
 	// check if seeds list contains target
 	for _, a := range seeds {
@@ -57,17 +57,17 @@ func TestRoutingNormVsSimple(t *testing.T) {
 			}
 			return false
 		})
-		// for i = 3 closest nodes in seeds, query nearestnodes on those rts
+		// for i = 3 closest nodesNormalizedRT in seeds, query nearestnodes on those rts
 		// append results to seeds list
 		for i := 0; i < 3; i++ {
 			// get the routing table of the node
-			for _, a := range nodes {
+			for _, a := range nodesNormalizedRT {
 				if a.NodeID == seeds[i] {
 					rt = a.RoutingTable
 					break
 				}
 			}
-			// get the 5 closest nodes to target from the routing table
+			// get the 5 closest nodesNormalizedRT to target from the routing table
 			// append to seeds list
 			seeds = append(seeds, rt.NearestNodes(target.Key(), 5)...)
 		}
@@ -84,8 +84,8 @@ func TestRoutingNormVsSimple(t *testing.T) {
 	}
 
 	// repeat process with simplert and count number of hops. then return difference.
-	rt = nodes_simple[0].RoutingTable
-	seeds_simple := rt.NearestNodes(target.Key(), 5) // 5 closest nodes to target <- change if needed for various experiments
+	rt = nodesSimpleRT[0].RoutingTable
+	seeds_simple := rt.NearestNodes(target.Key(), 5) // 5 closest nodesNormalizedRT to target <- change if needed for various experiments
 	targetFound = false
 	// check if seeds list contains target
 	for _, a := range seeds_simple {
@@ -108,17 +108,17 @@ func TestRoutingNormVsSimple(t *testing.T) {
 			}
 			return false
 		})
-		// for i = 3 closest nodes in seeds, query nearestnodes on those rts
+		// for i = 3 closest nodesNormalizedRT in seeds, query nearestnodes on those rts
 		// append results to seeds list
 		for i := 0; i < 3; i++ {
 			// get the routing table of the node
-			for _, a := range nodes_simple {
+			for _, a := range nodesSimpleRT {
 				if a.NodeID == seeds_simple[i] {
 					rt = a.RoutingTable
 					break
 				}
 			}
-			// get the 5 closest nodes to target from the routing table
+			// get the 5 closest nodesNormalizedRT to target from the routing table
 			// append to seeds list
 			seeds_simple = append(seeds_simple, rt.NearestNodes(target.Key(), 5)...)
 		}
