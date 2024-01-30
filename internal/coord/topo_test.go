c6475fafdf4365d63165e65b22eea8d7b09e3049
Miti Mazmudar <mitimazmudar@gmail.com>
2024-01-30 01:29:29 -0500

    topo_test.go: adding new doLookupSimplified

--- internal/coord/topo_test.go
+++ internal/coord/topo_test.go
@@ -21,10 +21,10 @@ func TestRoutingNormVsTrie(t *testing.T) {
 	require.NoError(t, err)
 
 	fmt.Println("Number of nodesTrieRT: ", len(nodesTrieRT))
-	//nodeIDs := make([]kadt.PeerID, len(nodesTrieRT))
-	//for i, node := range nodesTrieRT {
-	//	nodeIDs[i] = node.NodeID
-	//}
+	nodeIDs := make([]kadt.PeerID, len(nodesTrieRT))
+	for i, node := range nodesTrieRT {
+		nodeIDs[i] = node.NodeID
+	}
 
 	// select number from 0 to num_nodes-1 at random
 	// generate random integer between 0 and num_nodes-1
@@ -35,7 +35,7 @@ func TestRoutingNormVsTrie(t *testing.T) {
 	fmt.Println("target: ", target.String())
 
 	clientPeerID := nodesTrieRT[0].NodeID
-	hopCountTrie, err := doLookup(nodesTrieRT, target, clientPeerID)
+	hopCountTrie, err := doLookupSimplified(nodesTrieRT, nodeIDs, target, clientPeerID)
 	require.NoError(t, err)
 	fmt.Println("Trie: ", hopCountTrie)
 
@@ -50,14 +50,109 @@ func TestRoutingNormVsTrie(t *testing.T) {
 	// fmt.Println("Difference: ", hopCountNormalized-hopCountTrie)
 }
 
+func doLookupSimplified(nodes []*nettest.Peer, nodeIDs []kadt.PeerID, target kadt.PeerID, client kadt.PeerID) (int, error) {
+	var seeds []kadt.PeerID
+	var nearestNodes []kadt.PeerID
+	hopCount := 0
+	peersVisited := 0
+
+	// initialize seeds with results of running rt.NearestNodes on the client
+	//  note the client continues to use NearestNodes, instead of NearestNodesAsServer
+	index := slices.Index(nodeIDs, client)
+	if index == -1 {
+		return 0, fmt.Errorf("could not find one of the peers returned by a server in the global list of nodeIDs")
+	}
+	seeds = nodes[index].RoutingTable.NearestNodes(target.Key(), 20)
+	var seedsNextRound []kadt.PeerID
+
+	targetFound := slices.Contains(seeds, target)
+	if targetFound {
+		return 0, nil
+	}
+
+	for targetFound == false {
+		//for _, peerID := range seeds {
+		//	cpl := peerID.Key().CommonPrefixLength(target.Key())
+		//}
+		// sort items in seeds list by distance to target
+		sort.SliceStable(seeds, func(i, j int) bool {
+			distI := seeds[i].Key().Xor(target.Key())
+			distJ := seeds[j].Key().Xor(target.Key())
+
+			cmp := distI.Compare(distJ)
+			if cmp != 0 {
+				return cmp < 0
+			}
+			return false
+		})
+
+		// unique after ordering
+		for i := 1; i < len(seeds); i++ {
+			if seeds[i] == seeds[i-1] {
+				seeds = slices.Delete(seeds, i, i+1)
+			}
+		}
+
+		// pick top 20 from seeds
+		seeds = seeds[:20]
+
+		for _, peerID := range seeds {
+			cpl := peerID.Key().CommonPrefixLength(target.Key())
+			fmt.Println(cpl)
+		}
+		fmt.Printf("maxCPL %d\n", target.Key().CommonPrefixLength(seeds[0].Key()))
+
+		// as long as the target is not found,
+		// for all nodes in seeds, get their RTs, query nearestnodes on those rts
+		for i := 0; i < len(seeds) && !targetFound; i++ {
+			// get the routing table of the node
+			index := slices.Index(nodeIDs, seeds[i])
+			if index == -1 {
+				return 0, fmt.Errorf("could not find one of the peers returned by a server in the global list of nodeIDs")
+			}
+			rt := nodes[index].RoutingTable
+
+			// query nearestnodes on those rts
+			if rtNormalized, isRtNormalized := rt.(interface{}).(RoutingTableCplNormalized[kadt.Key, kadt.PeerID]); isRtNormalized {
+				nearestNodes = rtNormalized.NearestNodesAsServer(target.Key(), client.Key())
+			} else {
+				nearestNodes = rt.NearestNodes(target.Key(), 20)
+			}
+			if len(nearestNodes) < 20 {
+				return 0, fmt.Errorf("nearest nodes from node index %d returns less than 20 nodes: %d", index, len(nearestNodes))
+			}
+
+			peersVisited++
+
+			// set targetFound to whether the nearestNodes output by this last peer, includes the target
+			targetFound = slices.Contains(nearestNodes, target)
+			if targetFound {
+				fmt.Printf("target found on RT of node %s\n", nodes[index].NodeID.String())
+			}
+			// append to seeds list
+			seedsNextRound = append(seedsNextRound, nearestNodes...)
+		}
+
+		for i, seed := range seedsNextRound {
+			if slices.Contains(seeds, seed) {
+				seedsNextRound = slices.Delete(seedsNextRound, i, i+1)
+			}
+		}
+
+		seeds = seedsNextRound
+
+		hopCount++
+		fmt.Println(hopCount)
+	}
+	return hopCount, nil
+}
+
 func doLookup(nodes []*nettest.Peer, target kadt.PeerID, client kadt.PeerID) (int, error) {
 	// TODO: rt should, for the first lookup, refer to the client peer's rt.
 	//  Not just any random peer's RT for the seeds
 	rt := nodes[0].RoutingTable
 	var nearestNodes []kadt.PeerID
 	if rtNormalized, isRtNormalized := rt.(interface{}).(RoutingTableCplNormalized[kadt.Key, kadt.PeerID]); isRtNormalized {
-		// TODO: NearestNodesAsServer returns the full d.cfg.Bucketsize (20) number of elements --- doesn't currently have a tunable parameter
-		//  so I set the call to NearestNodes to return back 20
 		nearestNodes = rtNormalized.NearestNodesAsServer(target.Key(), client.Key())
 	} else {
 		nearestNodes = rt.NearestNodes(target.Key(), 20)
