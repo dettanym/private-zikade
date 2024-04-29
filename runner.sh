1edc384a883c0ed917ab642e6a27c946864b7f88
Miti Mazmudar <mitimazmudar@gmail.com>
2024-04-29 16:57:58 -0400

    refactor(runner.sh): add comment on output folders

--- runner.sh
+++ runner.sh
@@ -12,6 +12,7 @@ docker run --rm -ti \
   golang \
     #This will run our evaluation for peer routing
     # It will produce the first column of figures in Fig 1
+    # Output will be written to the csv_results folder
     go test \
       -test.v \
       -test.timeout 99999s \
@@ -20,6 +21,7 @@ docker run --rm -ti \
       ./pir/... && \
     #This will run our evaluation for provider advertisements
     # It will produce the second column of figures in Fig 1
+    # Output will be written to the csv_results folder
     go test \
       -test.v \
       -test.timeout 99999s \
@@ -29,6 +31,7 @@ docker run --rm -ti \
     #This will evaluate the normalized RT and the original RT
     # for difference in the number of hops (i.e. convergence),
     # as decribed in S5.2 'Routing table with normalized buckets'
+    # Output will go to stdout
     go test \
       -test.v \
       -test.timeout 99999s \
