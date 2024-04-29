05f3a528faaf8cdd7ec3312deee7a2da5147117a
Miti Mazmudar <mitimazmudar@gmail.com>
2024-04-29 16:49:53 -0400

    feat(runner.sh): run TestRoutingNormVsTrie

--- runner.sh
+++ runner.sh
@@ -10,15 +10,27 @@ docker run --rm -ti \
   -w "/code" \
   --user "$(id -u):$(id -g)" \
   golang \
+    #This will run our evaluation for peer routing
+    # It will produce the first column of figures in Fig 1
+    go test \
+      -test.v \
+      -test.timeout 99999s \
+      -test.bench '^\QBenchmark_PIR_for_Routing_Table\E$' \
+      -test.run '^$' \
+      ./pir/... && \
+    #This will run our evaluation for provider advertisements
+    # It will produce the second column of figures in Fig 1
     go test \
       -test.v \
       -test.timeout 99999s \
       -test.bench '^\QBenchmark_PIR_for_Provider_Routing\E$' \
       -test.run '^$' \
       ./pir/... && \
+    #This will evaluate the normalized RT and the original RT
+    # for difference in the number of hops (i.e. convergence),
+    # as decribed in S5.2 'Routing table with normalized buckets'
     go test \
       -test.v \
       -test.timeout 99999s \
-      -test.bench '^\Benchmark_PIR_for_Routing_Table\E$' \
-      -test.run '^$' \
-      ./pir/...
+      -test.run '^\QTestRoutingNormVsTrie\E$' \
+      ./internal/coord/
